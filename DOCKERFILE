8704373ee30eed7be8596be2d82aafe3280ec412
mahi-naidu <mahinaidu1432s@gmail.com>
2025-04-16 21:49:19 +0530

    updating python code

---
+++ DOCKERFILE
@@ -0,0 +1,10 @@
+FROM python:3.10-slim
+
+WORKDIR /app
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+ENV FLASK_APP=app.py
+
+CMD ["flask", "run", "--host=0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        database=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD']
+    )
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT version();')
+    db_version = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f'Connected to PostgreSQL: {db_version}'
